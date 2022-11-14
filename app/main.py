42c432e17f3921235632d86db0183941f78d505c
Haishuk Pavlo <xomas@ukr.net>
2022-11-14 21:42:27 +0200

    Revert "done"
    This reverts commit 117cd5b7d7b6e82f76f6a545b72dbb205eb3e2dd.

--- app/main.py
+++ app/main.py
@@ -1,26 +1,8 @@
 class Person:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        self.__class__.people[self.name] = self
+    # write your code here
+    pass
 
 
 def create_person_list(people: list) -> list:
-    instance_list = [
-        Person(person["name"], person["age"]) for person in people
-    ]
-    for man in people:
-        if "wife" in man and man["wife"] is not None:
-            for wife in people:
-                if man["wife"] == wife["name"]:
-                    Person.people[man["name"]].wife = \
-                        Person.people[wife["name"]]
-    for woman in people:
-        if "husband" in woman and woman["husband"] is not None:
-            for husband in people:
-                if woman["husband"] == husband["name"]:
-                    Person.people[woman["name"]].husband = \
-                        Person.people[husband["name"]]
-    return instance_list
+    # write your code here
+    pass
