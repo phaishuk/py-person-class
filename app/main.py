432817bad8f91af2bdda3fa2683971699d51a354
Haishuk Pavlo <xomas@ukr.net>
2022-11-15 11:45:50 +0200

    Finally, understood what do you mean ;)

--- app/main.py
+++ app/main.py
@@ -13,7 +13,9 @@ def create_person_list(people: list) -> list:
     ]
     for person in people:
         if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
         if "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
     return instance_list
